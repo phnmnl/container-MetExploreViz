1f66e68dcb99b1bfe21b72b6e7a985ecce03154e
Benjamin Merlet <benjamin.merlet@inra.fr>
2017-06-26 12:22:00 +0200

    Update for galaxy integration

--- deployApp4Galaxy.sh
+++ deployApp4Galaxy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 OUTPUTDIR=$1
 GRAPH_FILE=$2
-
 cp -r /opt/jsApp $OUTPUTDIR
 cp $GRAPH_FILE $OUTPUTDIR/mapped_graph.json
---
+++ runTest1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# initialize exit code to 0
+code=0
+
+echo "MetExplore Vizualisation tests finished without error codes"
